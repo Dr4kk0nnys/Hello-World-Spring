80d0cd6ff4c1e17fb26c5a55c2fa388f74d54083
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2022-04-21 04:38:17 -0300

    Greatly improved get user and create user

--- src/main/java/com/dr4kk0nnys/project/controllers/UserController.java
+++ src/main/java/com/dr4kk0nnys/project/controllers/UserController.java
@@ -1,21 +1,34 @@
 package com.dr4kk0nnys.project.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.dr4kk0nnys.project.models.User;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private Map<String, User> users = new HashMap<>();
 
     @PostMapping("users/create-user")
     public User createUser(@RequestBody User user) {
 
         User newUser = new User(user);
         System.out.println("New user created: " + newUser.getFullName());
+        users.put(newUser.getEmail(), newUser);
 
         return newUser;
     }
 
+    @GetMapping("users/get-user")
+    public User getUser(@RequestParam String email) {
+        return users.get(email);
+    }
 }
