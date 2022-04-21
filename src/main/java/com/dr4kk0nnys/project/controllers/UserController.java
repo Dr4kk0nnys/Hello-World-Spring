8e12d9302acd06f57d80b99cf332ddaa0b4bf3da
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2022-04-21 04:49:06 -0300

    Added update method to users controller

--- src/main/java/com/dr4kk0nnys/project/controllers/UserController.java
+++ src/main/java/com/dr4kk0nnys/project/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.dr4kk0nnys.project.controllers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +7,7 @@ import com.dr4kk0nnys.project.models.User;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +30,22 @@ public class UserController {
     @GetMapping("users/get-user")
     public User getUser(@RequestParam String email) {
         return users.get(email);
+    }
+
+    @PutMapping("users/update-user")
+    public User updateUser(@RequestParam String email, @RequestBody User newUser) {
+        User user = this.getUser(email);
+
+        if (user == null) {
+            return null;
+        }
+
+        user.setFullName(newUser.getFullName());
+        user.setEmail(newUser.getEmail());
+        user.setPassword(newUser.getPassword());
+
+        users.put(user.getEmail(), user);
+
+        return user;
     }
 }
